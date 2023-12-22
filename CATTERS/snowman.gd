ce17625d9451bedde30c8afef62b7a7bb1841c6a
Syyntakeeton <aleksi.jaaskelainen@outlook.com>
2023-12-22 13:33:43 +0200

    perse

--- CATTERS/snowman.gd
+++ CATTERS/snowman.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 
 const speed = 300.0
 const JUMP_VELOCITY = -400.0
+var hp = 2#? 2 lyöntiä/päälle hyppyä
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -22,3 +23,11 @@ func movement(delta):
 func turn_around():
 	if not $RayCast2D.is_colliding() and is_on_floor():
 		print("käänny")
+
+
+func _on_area_2d_area_entered(area):#pelaajan meleen hitboxi
+	print(area)
+
+
+func _on_area_2d_area_shape_entered(area_rid, area, area_shape_index, local_shape_index):
+	print(area)

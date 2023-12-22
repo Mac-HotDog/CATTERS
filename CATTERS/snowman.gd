b46fc44ad5d60cbf7fd9bd400cae40a58597e337
Syyntakeeton <aleksi.jaaskelainen@outlook.com>
2023-12-22 12:41:50 +0200

    fixejä

--- CATTERS/snowman.gd
+++ CATTERS/snowman.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
+const speed = 300.0
 const JUMP_VELOCITY = -400.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
@@ -9,21 +9,16 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
 func _physics_process(delta):
-	# Add the gravity.
+	movement(delta)
+
+
+
+func movement(delta):
 	if not is_on_floor():
 		velocity.y += gravity * delta
-
-#	# Handle Jump.
-#	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-#		velocity.y = JUMP_VELOCITY
-#
-#	# Get the input direction and handle the movement/deceleration.
-#	# As good practice, you should replace UI actions with custom gameplay actions.
-#	var direction = Input.get_axis("ui_left", "ui_right")
-#	if direction:
-#		velocity.x = direction * SPEED
-#	else:
-#		velocity.x = move_toward(velocity.x, 0, SPEED)
-	
-	
+	velocity.x = speed
 	move_and_slide()
+
+func turn_around():
+	if not $RayCast2D.is_colliding() and is_on_floor():
+		print("käänny")

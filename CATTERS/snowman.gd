5a0b1b8462ec2c5d260f4691ba56e20accad43f7
Syyntakeeton <aleksi.jaaskelainen@outlook.com>
2023-12-22 14:07:25 +0200

    vittu

--- CATTERS/snowman.gd
+++ CATTERS/snowman.gd
@@ -1,18 +1,28 @@
 extends CharacterBody2D
 
 
-const speed = 300.0
-const JUMP_VELOCITY = -400.0
+const speed = 150.0
+const JUMP_VELOCITY = -300.0
+var died = false
 var hp = 2#? 2 lyöntiä/päälle hyppyä
+@onready var animation_player = $AnimationPlayer
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
 func _physics_process(delta):
+	if hp == 0:
+		dead()
 	movement(delta)
+	
+func play_animation(animation):
+	if animation:
+		animation_player.play(animation)
 
-
+func dead():
+	died = true
+	play_animation("dead")
 
 func movement(delta):
 	if not is_on_floor():
@@ -26,8 +36,10 @@ func turn_around():
 
 
 func _on_area_2d_area_entered(area):#pelaajan meleen hitboxi
+	#if area.get_parent() 
+		#print("lyönti")
 	print(area)
+	hp = -1
 
 
-func _on_area_2d_area_shape_entered(area_rid, area, area_shape_index, local_shape_index):
-	print(area)
+
